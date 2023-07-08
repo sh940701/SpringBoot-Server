6859f36a8c40e5b6f140920a48dd5c7586d08c9d
96sunghyun <visioner2168@gmail.com>
2023-07-08 18:33:20 +0900

    feat: add getter, setter for _id

--- src/main/java/com/example/project/model/Board.java
+++ src/main/java/com/example/project/model/Board.java
@@ -1,11 +1,24 @@
 package com.example.project.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "board")
 public class Board {
+    @Id
     private String id;
     private String title;
     private String content;
 
     // Getters and Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
